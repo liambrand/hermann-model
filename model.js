9547b8c9025d5fa1fdb5b0d0abf2609a592ed46c
liambrand <liam.brand@northumbria.ac.uk>
2019-03-10 12:56:17 +0000

    Add files via upload

--- model.js
+++ model.js
@@ -1,6 +1,8 @@
-var device1Url = 'https://m9drg2gpya.execute-api.us-east-2.amazonaws.com/default/getRequest?deviceid=1';
-var device2Url = 'https://m9drg2gpya.execute-api.us-east-2.amazonaws.com/default/getRequest?deviceid=2';
+var readingsUrl = 'https://m9drg2gpya.execute-api.us-east-2.amazonaws.com/default/getRequest';
+var userid = 1; // should change based on current session
 var key = 'sS6BW9aZNY1NMndVf6s9V6LegVVn7WPj5xuVnq9b';
+
+var device = 1;
 
 var device1Temps = [];
 var device1Humidities = [];
@@ -8,143 +10,106 @@ var device2Temps = [];
 var device2Humidities = [];
 
 /* CHECK TEMPERATURES */
-function checkDevice1Temp() {
-    // CurrentTemp will be retrieved from the device
-    var currentTemp = device1Temps[0].sensor_value;
-    console.log(currentTemp);
-    // Device will retrieve a few previous readings from similar dates
-    var previousTemp1 = device1Temps[1].sensor_value;
-    var previousTemp2 = device1Temps[2].sensor_value;
-    var previousTemp3 = device1Temps[3].sensor_value;
-    var previousTemp4 = device1Temps[4].sensor_value;
-    // From these retrieved temps it will work out a happy medium
-    var preferredTemp = ((previousTemp1 + previousTemp2 + previousTemp3 + previousTemp4) /4);
+function checkTemps(deviceid, temps) {
+    
+    // Get current temperature timestamp
+    let currentTemp = temps[0];
+    
+    // Get timestamps for previous days at the same time
+    let today = new Date(currentTemp.timestamp);
+    let oneDayAgo = dateFns.subDays(today, 1);
+    let twoDaysAgo = dateFns.subDays(today, 2);
+    /*console.log(today);
+    console.log(oneDayAgo);
+    console.log(twoDaysAgo);*/
+    
+    /* here we will return readings from previous dates, put them into
+       variables and find which readings are the closest to our created timestamps */
+    
+    var dates = [];
+    for(var i = 0; i < temps.length; i++) {
+        dates = dates.concat(temps[i].timestamp);
+    }
+    // yesterdays, the day befores, maybe a year or two ago as well
+    let oneDayAgoTemp = temps[dateFns.closestIndexTo(oneDayAgo, dates)].sensor_value;
+    let twoDaysAgoTemp = temps[dateFns.closestIndexTo(twoDaysAgo, dates)].sensor_value;
+    
+    avgTemp = (oneDayAgoTemp + twoDaysAgoTemp)/2;
     
     // Here we have a small buffer rather than needing to match the temperature perfectly
-    if((currentTemp > preferredTemp - 4) && (currentTemp < preferredTemp + 4)) {
-        $('#device1TempStatus').html("Temperature OK!");
+    if(currentTemp.sensor_value > avgTemp + 3) {
+        $('#device' + deviceid + 'TempStatus').html("Too Hot!");
     }
-    else if(currentTemp > preferredTemp) {
-        $('#device1TempStatus').html("Too Hot!");
+    else if(currentTemp.sensor_value < avgTemp - 3) {
+        $('#device' + deviceid + 'TempStatus').html("Too Cold!");
     }
     else {
-        $('#device1TempStatus').html("Too Cold!");
-    }  
+        $('#device' + deviceid + 'TempStatus').html("Temperature OK!");
+    }
 }
 
-function checkDevice2Temp() {
-    // CurrentTemp will be retrieved from the device
-    var currentTemp = device2Temps[0].sensor_value;
-    // Device will retrieve a few previous readings from similar dates
-    var previousTemp1 = device2Temps[1].sensor_value;
-    var previousTemp2 = device2Temps[2].sensor_value;
-    var previousTemp3 = device2Temps[3].sensor_value;
-    var previousTemp4 = device2Temps[4].sensor_value;
-    // From these retrieved temps it will work out a happy medium
-    var preferredTemp = ((previousTemp1 + previousTemp2 + previousTemp3 + previousTemp4) /4);
+/* CHECK TEMPERATURES */
+function checkHumidity(deviceid, humidities) {
+    
+    // Get current temperature timestamp
+    let currentHumidity = humidities[0];
+    
+    // Get timestamps for previous days at the same time
+    let today = new Date(currentHumidity.timestamp);
+    let oneDayAgo = dateFns.subDays(today, 1);
+    let twoDaysAgo = dateFns.subDays(today, 2);
+    
+    /* here we will return readings from previous dates, put them into
+       variables and find which readings are the closest to our created timestamps */
+    
+    var dates = [];
+    for(var i = 0; i < humidities.length; i++) {
+        dates = dates.concat(humidities[i].timestamp);
+    }
+    // yesterdays, the day befores, maybe a year or two ago as well
+    let oneDayAgoHumidity = humidities[dateFns.closestIndexTo(oneDayAgo, dates)].sensor_value;
+    let twoDaysAgoHumidity = humidities[dateFns.closestIndexTo(twoDaysAgo, dates)].sensor_value;
+    
+    avgHumidity = (oneDayAgoHumidity + twoDaysAgoHumidity)/2;
     
     // Here we have a small buffer rather than needing to match the temperature perfectly
-    if((currentTemp > preferredTemp - 4) && (currentTemp < preferredTemp + 4)) {
-        $('#device2TempStatus').html("Temperature OK!");
+    if(currentHumidity.sensor_value > avgHumidity + 3) {
+        $('#device' + deviceid + 'HumidityStatus').html("Too Humid!");
     }
-    else if(currentTemp > preferredTemp) {
-        $('#device2TempStatus').html("Too Hot!");
-    }
-    else {
-        $('#device2TempStatus').html("Too Cold!");
-    }  
-}
-
-/* CHECK HUMIDITIES */
-function checkDevice1Humidity() {
-    // CurrentTemp will be retrieved from the device
-    var currentHumidity = device1Temps[0].sensor_value;
-    // Device will retrieve a few previous readings from similar dates
-    var previousHumidity1 = device1Humidities[1].sensor_value;
-    var previousHumidity2 = device1Humidities[2].sensor_value;
-    var previousHumidity3 = device1Humidities[3].sensor_value;
-    var previousHumidity4 = device1Humidities[4].sensor_value;
-    // From these retrieved temps it will work out a happy medium
-    var preferredHumidity = ((previousHumidity1 + previousHumidity2 + previousHumidity3 + previousHumidity4) /4);
-    
-    // Here we have a small buffer rather than needing to match the temperature perfectly
-    if((currentHumidity > preferredHumidity - 4) && (currentTemp < preferredTemp + 4)) {
-        $('#device1HumidityStatus').html("Humidity OK!");
-    }
-    else if(currentHumidity > preferredHumidity) {
-        $('#device1HumidityStatus').html("Too Humid!");
+    else if(currentHumidity.sensor_value < avgHumidity - 3) {
+        $('#device' + deviceid + 'HumidityStatus').html("Too Dry!");
     }
     else {
-        $('#device1HumidityStatus').html("Humidity Low!");
-    } 
-}
-
-function checkDevice2Humidity() {
-    // CurrentTemp will be retrieved from the device
-    var currentHumidity = device2Temps[0].sensor_value;
-    // Device will retrieve a few previous readings from similar dates
-    var previousHumidity1 = device2Humidities[1].sensor_value;
-    var previousHumidity2 = device2Humidities[2].sensor_value;
-    var previousHumidity3 = device2Humidities[3].sensor_value;
-    var previousHumidity4 = device2Humidities[4].sensor_value;
-    // From these retrieved temps it will work out a happy medium
-    var preferredHumidity = ((previousHumidity1 + previousHumidity2 + previousHumidity3 + previousHumidity4) /4);
-    
-    // Here we have a small buffer rather than needing to match the temperature perfectly
-    if((currentHumidity > preferredHumidity - 4) && (currentTemp < preferredTemp + 4)) {
-        $('#device2HumidityStatus').html("Humidity OK!");
-    }
-    else if(currentHumidity > preferredHumidity) {
-        $('#device2HumidityStatus').html("Too Humid!");
-    }
-    else {
-        $('#device2HumidityStatus').html("Humidity Low!");
-    } 
-}
-
-/* COMPARE DEVICES */
-function compareTemps() {
-    var averageDevice1Temp =
-        ((device1Temps[0] +
-         device1Temps[1] +
-         device1Temps[2] +
-         device1Temps[3] +
-         device1Temps[4] +
-         device1Temps[5])/5)
-    
-    var averageDevice2Temp =
-        ((device2Temps[0] +
-         device2Temps[1] +
-         device2Temps[2] +
-         device2Temps[3] +
-         device2Temps[4] +
-         device2Temps[5])/5)
-    
-    var diff = averageDevice1Temp - averageDevice2Temp;
-    
-    if((diff > 4) || (diff < -4)) {
-        $('#deviceStatus').html('Discrepancy Detected');
-    }
-    else {
-        $('#deviceStatus').html('Devices OK');
+        $('#device' + deviceid + 'HumidityStatus').html("Humidity OK!");
     }
 }
 
-/* CHECK DEVICE1 BEHAVIOUR */
+/* CHECK DEVICE CONSISTENCY */
+// Compared each devices readings to eachother over a 2 week period
+function checkDeviceConsistency(device1Readings, device2Readings, sensorType) {
+    today = new Date();
+    twoWeeksAgo = dateFns.subWeeks(today, 2);
+    console.log(twoWeeksAgo);
+    
+}
+
+
+
+/* CHECK DEVICE BEHAVIOUR */
 function checkDeviceBehaviour() {
     
 }
 
-/* CHECK DEVICE2 BEHAVIOUR */
 
-
-
-/* PERIODICALLY GET DEVICE 1 READINGS */
-(function readDevice1() {
+/* PERIODICALLY GET DEVICE READINGS */
+(function getUserData() {
   $.ajax({
-    url: device1Url,
+    url: readingsUrl,
     headers: {
       "X-Api-Key":key,
+    },
+    data: {
+        'user_id': userid
     },
     success: function(data) {
         // Sort the returned data to ensure we are using the latest readings for our calculations
@@ -154,57 +119,37 @@ function checkDeviceBehaviour() {
             return new Date(b.timestamp) - new Date(a.timestamp);
         });
         
-        // Sort the returned data into humidity and temperature arrays
-        for(var i = 0; i < data.length; i++) {
-            if(data[i].sensor_type == "TEMP") {
-                device1Temps = device1Temps.concat(data[i]);
-            }
-            else if(data[i].sensor_type == "HUMIDITY") {
-                device1Humidities = device1Humidities.concat(data[i]);
-            }
-        }
-        checkDevice1Temp();
-        checkDevice1Humidity();
-        compareTemps();
-    },
-    complete: function() {
-      // Schedule the next request when the current one's complete
-      //setTimeout(worker, 60000);
-    }
-  });
-})();
-
-/* PERIODICALLY GET DEVICE 2 READINGS */
-(function readDevice2() {
-  $.ajax({
-    url: device2Url,
-    headers: {
-      "X-Api-Key":key,
-    },
-    success: function(data) {
-        // Sort the returned data to ensure we are using the latest readings for our calculations
-        data.sort(function(a,b){
-            // Turn your strings into dates, and then subtract them
-            // to get a value that is either negative, positive, or zero.
-            return new Date(b.timestamp) - new Date(a.timestamp);
-        });
         
         // Sort the returned data into humidity and temperature arrays
         for(var i = 0; i < data.length; i++) {
             if(data[i].sensor_type == "TEMP") {
-                device2Temps = device2Temps.concat(data[i]);
+                if(data[i].device_id == 1) {
+                    device1Temps = device1Temps.concat(data[i])
+                }
+                else {
+                    device2Temps = device2Temps.concat(data[i])
+                }
             }
             else if(data[i].sensor_type == "HUMIDITY") {
-                device2Humidities = device2Humidities.concat(data[i]);
+                if(data[i].device_id == 1) {
+                    device1Humidities = device1Humidities.concat(data[i])
+                }
+                else {
+                    device2Humidities = device2Humidities.concat(data[i])
+                }
             }
         }
-        checkDevice2Temp();
-        checkDevice2Humidity();
-        compareTemps();
+        checkTemps(1, device1Temps);
+        checkTemps(2, device2Temps);
+        
+        checkHumidity(1, device1Humidities);
+        checkHumidity(2, device2Humidities);
+        
+        checkDeviceConsistency(device1Temps, device2Temps, 'TEMP');
     },
     complete: function() {
       // Schedule the next request when the current one's complete
-      //setTimeout(worker, 60000);
+      //setTimeout(getUserData, 60000); !!! UNCOMMENT THIS !!!
     }
   });
 })();
